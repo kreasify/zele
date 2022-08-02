a433b06ef4ad6165484176c498c44f00d54f25d7
Akhlis <kreasify@gmail.com>
2022-08-03 01:09:54 +0700

    post

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  base: '',
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        team: resolve(__dirname, 'src/team/index.html')
+      }
+    }
+  }
+})
