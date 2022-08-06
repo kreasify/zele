481b1ca9c812f64d21da28cb7519262a6f53d035
Akhlis <kreasify@gmail.com>
2022-08-06 14:51:52 +0700

    post

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {},
+  theme: {
+    extend: {
+      keyframes: {
+        fadeInDown: {
+          '0%': {
+            opacity: '0',
+            transform: 'translate3d(0, -200%, 0)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'none'
+          },
+        },
+      }
+    }
+  },
   plugins: [],
 }
